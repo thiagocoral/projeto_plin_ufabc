1415d9ee1a6e2c3f39b05d918a08a0a6d64f5bb1
thiagocoral <thi.coral@gmail.com>
2021-11-16 20:42:18 -0300

    upload do trabalho atual

---
+++ classifier.py
@@ -0,0 +1,139 @@
+import json
+from nltk.corpus.reader import wordlist
+import pandas as pd
+import numpy as np
+import random as rand
+import nltk
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+
+
+caminho = "C:/Users/thico/Machine Learning/PLIN/dfs_pedrao/dfs_textsCarille_using11.json"
+df_a_ser_testado = "df_text_representante"
+
+
+#df_text_representante
+#df_text_full
+
+file = open(caminho,encoding='utf-8')
+file_json = json.load(file)
+
+dict = file_json["texts"]
+
+#######
+####### REALIZAR STEMMING
+#######
+
+
+#gerando bag of words
+target_list = []
+word_list = []
+
+
+for register in dict:
+    
+    df = pd.DataFrame.from_dict(register[df_a_ser_testado])
+    for word in df.Type:
+        if word not in word_list:
+            word_list.append(word)
+    target_list.append(int(register["target"][:-1]))
+
+
+
+#gerando arrays com as frequencias
+array_all_frequencies = []
+for register in dict:
+    array_register = np.zeros(len(word_list))
+    df = pd.DataFrame.from_dict(register[df_a_ser_testado])
+    for i in range(len(df)):
+        array_register[word_list.index(df.iloc[i]["Type"])] = df.iloc[i]["Frequência(f)"]
+    array_register = tuple(array_register)
+    array_all_frequencies.append(array_register)
+
+
+#shuffle default = True
+x_treino, x_teste,y_treino,y_teste = train_test_split(array_all_frequencies,target_list,test_size=0.4,shuffle=False)
+
+classifier = GaussianNB()
+classifier.fit(x_treino, y_treino)
+
+# Predict Class
+y_pred = classifier.predict(x_teste)
+
+# Accuracy 
+accuracy = accuracy_score(y_teste, y_pred)
+print("GaussianNB", accuracy)
+
+classifier = SVC()
+classifier.fit(x_treino, y_treino)
+
+# Predict Class
+y_pred = classifier.predict(x_teste)
+
+# Accuracy 
+accuracy = accuracy_score(y_teste, y_pred)
+print("SVC:",accuracy)
+
+
+from sklearn import tree
+classifier = tree.DecisionTreeClassifier()
+classifier.fit(x_treino, y_treino)
+
+# Predict Class
+y_pred = classifier.predict(x_teste)
+
+# Accuracy 
+accuracy = accuracy_score(y_teste, y_pred)
+print("tree:",accuracy)
+
+
+############################################################ XGBOOST
+from sklearn.ensemble import GradientBoostingClassifier
+#subsample baixo = maior distribuição de feature_importance
+classifier = GradientBoostingClassifier(n_estimators=75, learning_rate=0.1,max_depth=15, random_state=0,subsample=0.6)
+classifier.fit(x_treino, y_treino)
+
+# Predict Class
+y_pred = classifier.predict(x_teste)
+
+# Accuracy 
+accuracy = accuracy_score(y_teste, y_pred)
+print("GradTrees:",accuracy)
+importancia_features = np.array(classifier.feature_importances_)
+nome_features = word_list
+dados = {"nome_features":nome_features,"importancia_features":importancia_features}
+dados_df = pd.DataFrame(dados)
+dados_df.sort_values(by=["importancia_features"],ascending=False,inplace=True)
+dados_df = dados_df.head(50)
+
+
+plt.figure(figsize=(8,6))
+sns.color_palette("husl", 9)
+sns.barplot(x=dados_df['importancia_features'], y=dados_df['nome_features'])
+plt.show()
+
+
+################################################### MLP
+from sklearn.neural_network import MLPClassifier
+classifier = MLPClassifier()
+classifier.fit(x_treino, y_treino)
+
+# Predict Class
+y_pred = classifier.predict(x_teste)
+
+# Accuracy 
+accuracy = accuracy_score(y_teste, y_pred)
+print("MLP:",accuracy)
+
+
+# from lazypredict.Supervised import LazyRegressor
+# clf = LazyRegressor()
+# models,predictions = clf.fit(x_treino, x_teste, y_treino, y_teste)
+
+# print(models)
+
---
+++ codigo-textos-token-plin.py
@@ -0,0 +1,93 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import nltk
+from nltk.corpus import stopwords
+nltk.download('punkt')
+nltk.download('stopwords')
+from nltk import word_tokenize
+import pandas as pd
+import json
+import os
+import numpy as np
+
+def extract_df(url):
+
+    page = requests.get(url)
+
+    web_scraped = BeautifulSoup(page.text,"html.parser")
+
+    #tech_info = web_scraped.find(id="content-text__container")
+    string_text = ""
+    string_representante = ""
+    for p in web_scraped.find_all("p",class_="content-text__container"):
+        string_text += p.text.lower()
+        if p.text[1] == "–":
+            string_representante += p.text.lower()
+
+
+    def tokenizar_texto(string_tonekiner):
+        tokens=word_tokenize(string_tonekiner) 
+
+        tokens_without_sw = [word for word in tokens if not word.lower() in stopwords.words()]
+        tokens_palavras=[w for w in tokens_without_sw if w.isalpha()]
+        fdist=nltk.FreqDist(string_tonekiner)
+        fdist_sort=fdist.most_common(50)
+
+        ######### LEI DE ZIPF ##########
+        num_types=len(set(tokens_palavras))
+
+        fdist=nltk.FreqDist(tokens_palavras)
+        fdist_sort=fdist.most_common(num_types)
+
+        # cria uma lista dos types
+        types=[w for (w,f) in fdist_sort ]
+
+        # cria uma lista com as frequências
+        freqs=[f for (w,f) in fdist_sort ]
+
+        # cria uma lista com as posições no ranking
+        ranks=list(range(1,num_types+1))
+
+        # calcula o produto f*r
+        produto= np.array(freqs)*np.array(ranks)
+
+        df = pd.DataFrame(list(zip(types, freqs, ranks, produto)),
+                        columns =['Type','Frequência(f)','Posto(r)',"f*r"])
+        return df
+
+    df_text_full = tokenizar_texto(string_text)
+    df_text_representante = tokenizar_texto(string_representante)
+    return df_text_full,df_text_representante
+
+
+
+def extract_all_urls():
+    with open("PLNLinks.txt","r") as url_file:
+        lines = url_file.readlines()
+        for line in lines:
+            line,target = line.split(",")
+            df_text_full,df_text_representante = extract_df(line[0:len(line)])
+            print(df_text_full,df_text_representante)
+
+            ### Verifica se o arquivo existe, se não ele é criado
+            if not os.path.isfile("dfs_texts.json"):
+                start_data = {
+                    "texts": []
+                }
+                with open("dfs_texts.json","w",encoding='utf-8') as file:
+                    json.dump(start_data,file)
+                print("file dfs_texts.json created")
+
+            ### Função de escrever os DFs no JSON
+            def write_to_json():
+                with open("dfs_texts.json","r+", encoding='utf-8') as file:
+                    full_file = json.load(file)
+                    new_data = {"url":line[0:len(line)-1],"df_text_full":df_text_full.to_dict(),"df_text_representante":df_text_representante.to_dict(),"target":target}
+                    full_file["texts"].append(new_data)
+                    file.seek(0)
+                    json.dump(full_file, file, indent = 4, ensure_ascii=False)
+
+            write_to_json()
+
+extract_all_urls()
